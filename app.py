b4fcd23fec0f5eb37090f438bcc45dbe89497f03
au421621104075 <rlavanyaramesh1808@gmail.com>
2025-12-01 23:35:40 +0530

    add the requriement,procfile,app.py port for host

--- app.py
+++ app.py
@@ -143,4 +143,6 @@ def reset_bookings():
 #http://127.0.0.1:5000/reset-bookings
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use Render's port or default 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
